b3adfc6d42a5c2b5150fa22f60581c0cce6133a9
Carlos Alberto Lara Hernandez <3sslara@gmail.com>
2025-11-18 11:29:21 -0600

    Insertion Sort

---
+++ Basics/Insertion Sort/Insertion Sort.cpp
@@ -0,0 +1,65 @@
+// #pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using ii = pair<int, int>;
+using vii = vector<ii>;
+
+#define sz(x) int(x.size())
+#define fi first
+#define se second
+#define pb emplace_back
+#define edl '\n'
+
+constexpr long long LLINF = 2e18;
+constexpr int INF = 2e9;
+constexpr int MOD = 1e9 + 7;
+constexpr int MxN = 1e6 + 5;
+constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+void solve() {
+    int n;
+    cin >> n;
+    vi arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    auto printv = [&]() {
+        cout << arr[0];
+        for (int i = 1; i < n; i++) {
+            cout << ' ' << arr[i];
+        }
+        cout << edl;
+    };
+    printv();
+    int key, j;
+    for (int i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+        printv();
+    }
+}
+
+int main() {
+    // freopen("text.in", "r", stdin);
+    // freopen("text.out", "w", stdout);
+    ios_base::sync_with_stdio(false), cin.tie(nullptr); // Fast I/O Setup
+    int q = 1;
+    // cin >> q;
+    while (q--) {
+        solve();
+    }
+    return 0;
+}
+// By KaarLarax
